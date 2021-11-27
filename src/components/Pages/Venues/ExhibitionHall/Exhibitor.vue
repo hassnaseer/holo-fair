46f2514b97b5a510acb6bdf3edd9736215614759
malikhassam <malik.hassam531@gmail.com>
2021-11-27 23:10:10 +0500

    exhibtor design

--- src/components/Pages/Venues/ExhibitionHall/Exhibitor.vue
+++ src/components/Pages/Venues/ExhibitionHall/Exhibitor.vue
@@ -1,14 +1,14 @@
 <template>
 <v-container fluid>
     <div class="row">
-        <div class="col-lg-4 col-md-6 col-sm-12">
+        <div class="col-lg-12 col-md-12 col-sm-12">
         <span>
         Upload Exhibitor booth content Below.
       </span>
         </div>
-                <div class="col-lg-4 col-md-6 col-sm-12">
+                <div class="col-lg-12 col-md-12 col-sm-12">
         <span>
-        Upload Exhibitor booth content Below.
+        If you don't have Exhibitor booth or stands, leave this section empty.
       </span>
         </div>
         <div class="col-lg-4 col-md-6 col-sm-12">
@@ -68,6 +68,31 @@
                 </div>
               </a-upload>
         </div>
+        <div class="col-lg-12 col-md-12 col-sm-12">
+  <a-upload-dragger
+    name="file"
+    :multiple="true"
+    action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+    @change="handleChange"
+  >
+    <p class="ant-upload-drag-icon">
+      <a-icon type="inbox" />
+    </p>
+    <p class="ant-upload-text">
+      Click or drag file to this area to upload
+    </p>
+  </a-upload-dragger>
+        </div>
+          <v-col col="12" lg="12" md="10" sm="10">
+    <h3>Max.Upload Size is : 2MB,</h3>
+    <h3>Recommended size is 2000* 800 px (5:2)</h3>
+  </v-col>
+<v-col cols="2" class="text-center">
+          <v-btn :loading="loading" type="submit" color="light-blue darken-2 px-8" dark> Cancel</v-btn>
+        </v-col>
+        <v-col cols="2" class="text-center">
+          <v-btn :loading="loading" type="submit" color="light-blue darken-2 px-8" dark> Save</v-btn>
+        </v-col>
         </div>
 </v-container>
 
@@ -115,8 +140,7 @@ export default {
 </script>
 <style>
 .avatar-uploader > .ant-upload {
-  width: 128px;
-  height: 128px;
+  width: 100%;
 }
 .ant-upload-select-picture-card i {
   font-size: 32px;
