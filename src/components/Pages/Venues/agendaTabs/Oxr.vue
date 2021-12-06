343b523e4954713476096a6344d1d9df4b7fbc0c
malikhassam <malik.hassam531@gmail.com>
2021-12-06 23:18:07 +0500

    agenda page

--- src/components/Pages/Venues/agendaTabs/Oxr.vue
+++ src/components/Pages/Venues/agendaTabs/Oxr.vue
@@ -1,45 +1,237 @@
 <template>
-<div class="row">
-<div class="col-lg-12">
-  <h1>
-Below you find questions from your atendees. Please share these queries, so that they may anser them during their live sessions.
-  </h1>
-</div>
-<div class="col-lg-12 mx-auto">
-  <v-card
-    class="mx-auto"
-    max-width="344"
-    outlined
-    hover
-  >
-    <v-list-item three-line>
-      <v-list-item-content>
-        <div class="text-overline mb-4">
-          OVERLINE
-        </div>
-        <v-list-item-title class="text-h5 mb-1">
-          Headline 5
-        </v-list-item-title>
-        <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
-      </v-list-item-content>
+  <v-container fluid>
+    <v-row>
+      <v-col lg="12" md="12" sm="12">
+        <v-data-table
+            :headers="headers"
+            :items="data"
+            :color="getColor(carbs)"
+            sort-by="calories"
+            class="elevation-1"
+        >
+          <template v-slot:top>
+            <v-toolbar
+                flat
+            >
+              <v-dialog
+                  v-model="dialog"
+                  max-width="500px"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn
+                      color="primary"
+                      dark
+                      class="mb-2"
+                      v-bind="attrs"
+                      v-on="on"
+                  >
+                    New Date
+                  </v-btn>
+                </template>
+                <v-card>
+                  <v-card-title>
+                    <span class="text-h5">{{ formTitle }}</span>
+                  </v-card-title>
 
-      <v-list-item-avatar
-        tile
-        size="80"
-        color="grey"
-      ></v-list-item-avatar>
-    </v-list-item>
+                  <v-card-text>
+                    <v-container>
+                      <v-row>
+                        <v-col
+                            cols="12"
+                            sm="6"
+                            md="6"
+                        >
+                          <v-text-field
+                              v-model="editedItem.roleName"
+                              label="Role"
+                          ></v-text-field>
 
-    <v-card-actions>
-      <v-btn
-        outlined
-        rounded
-        text
-      >
-        Button
-      </v-btn>
-    </v-card-actions>
-  </v-card>
-</div>
-</div>
+                        </v-col>
+                      </v-row>
+                    </v-container>
+                  </v-card-text>
+
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn
+                        color="blue darken-1"
+                        text
+                        @click="close"
+                    >
+                      Cancel
+                    </v-btn>
+                    <v-btn
+                        color="blue darken-1"
+                        text
+                        @click="save"
+                    >
+                      Save
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+              <v-dialog v-model="dialogDelete" max-width="500px">
+                <v-card>
+                  <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+                    <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+                    <v-spacer></v-spacer>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+            </v-toolbar>
+          </template>
+          <template v-slot:item.actions="{ item }">
+          <v-btn :loading="loading" type="submit" color="light-blue darken-2 px-8" dark> Select</v-btn>
+            </template>
+          <template v-slot:no-data>
+            <span
+                color="primary"
+                @click="initialize"
+            >
+              No Record Found
+            </span>
+          </template>
+        </v-data-table>
+      </v-col>
+    </v-row>
+  </v-container>
+
 </template>
+<script>
+import axios from "axios";
+
+export default {
+  beforeRouteEnter(to, from, next) {
+    const token = localStorage.getItem('token')
+    if (token) {
+      next()
+    } else
+      next('/login')
+  },
+  data: () => ({
+    data: [],
+    dialog: false,
+    dialogDelete: false,
+    headers: [
+      {text: 'Roles', value: 'roleName'},
+      {text: 'Actions', value: 'actions', sortable: false},
+    ],
+    desserts: [],
+    editedIndex: -1,
+    editedItem: {
+      roleName: '',
+    },
+    defaultItem: {
+      roleName: '',
+    },
+  }),
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? 'New Role' : 'Edit Role'
+    },
+  },
+
+  watch: {
+    dialog(val) {
+      val || this.close()
+    },
+  },
+
+  created() {
+    this.initialize()
+  },
+  mounted() {
+    this.getRoles();
+  },
+  methods: {
+    async getRoles() {
+      try {
+        const response = await axios.get(
+            `${process.env.VUE_APP_SERVER_URL}/api/v1/attendee-roles`
+        );
+        this.data = response.data.data;
+      } catch (error) {
+        alert(error);
+      }
+    },
+    getColor(carbs) {
+      if (carbs === "Active") return 'red'
+      else if (carbs === "Inactive") return 'orange'
+      else return 'green'
+    },
+    initialize() {
+      this.desserts = [
+        {
+          name: 'Anurag',
+          fat: 'anurag@outrealxr.com',
+          roles: 'admin',
+        },
+        {
+          name: 'Anurag',
+          fat: 'anurag@outrealxr.com',
+          roles: 'client',
+        },
+      ]
+    },
+    editItem(item) {
+      this.editedIndex = this.data.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialog = true
+    },
+
+    deleteItem(item) {
+      this.editedIndex = this.data.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialogDelete = true
+    },
+
+    deleteItemConfirm() {
+      this.data.splice(this.editedIndex, 1)
+      this.closeDelete()
+    },
+
+    close() {
+      this.dialog = false
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
+    },
+
+    closeDelete() {
+      this.dialogDelete = false
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
+    },
+
+    async save() {
+      try {
+        alert(JSON.stringify(this.editedItem));
+        if (this.formTitle === "New Role") {
+          await axios.post(`${process.env.VUE_APP_SERVER_URL}/api/v1/attendee-role`, {
+            ...this.editedItem, operation: "C"
+          });
+          // alert(res.data.meta.message)
+        } else {
+          await axios.post(`${process.env.VUE_APP_SERVER_URL}/api/v1/attendee-role`, {
+            ...this.editedItem, operation: "E"
+          });
+        }
+        if (this.editedIndex > -1) {
+          Object.assign(this.data[this.editedIndex], this.editedItem)
+        } else {
+          this.data.push(this.editedItem)
+        }
+        this.close()
+      } catch (er) {
+        alert("Error: " + er.message)
+      }
+    }
+  }
+}
+</script>
